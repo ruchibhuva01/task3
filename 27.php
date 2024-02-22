57234409605ca39e1fca26945a45799d3ba155aa
ruchibhuva01 <155124370+ruchibhuva01@users.noreply.github.com>
2024-02-22 08:35:11 +0530

    Update 27.php

--- 27.php
+++ 27.php
@@ -1,5 +1,5 @@
 <?php
-$student=array("name"=>"sarjita","age"=>20,"city"=>"bangavdi");
-$value=array("sname"=>"komal","sage"=>34,"scity"=>"bangavdi");
+$student=array("name"=>"ruchi","age"=>20,"city"=>"Gondal");
+$value=array("sname"=>"Disha","sage"=>25,"scity"=>"Rajkot");
 print_r(array_merge($student,$value));
 ?>
