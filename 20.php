74fd0c74de9cc35dc8a5daaf9902cd7322a9c6c0
ruchibhuva01 <155124370+ruchibhuva01@users.noreply.github.com>
2024-02-22 08:31:08 +0530

    Update 20.php

--- 20.php
+++ 20.php
@@ -1,5 +1,5 @@
 <?php
-$student=array("name"=>"sarjita","age"=>23,"city"=>"balapar");
+$student=array("name"=>"Ruchi","age"=>20,"city"=>"Gondal");
 $value=array_flip($student);
 print_r($value);
 ?>
